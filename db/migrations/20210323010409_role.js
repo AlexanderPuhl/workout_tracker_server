dcd560f016c2625fee4d6ea3b6781eba3fe2505f
Alex Puhl <alexpuhldeveloper@gmail.com>
2021-07-25 16:45:26 -0600

    Put user_id back into various tables, updated seed files accordingly

--- db/migrations/20210323010409_role.js
+++ db/migrations/20210323010409_role.js
@@ -2,7 +2,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('role', (table) => {
     table.increments('role_id').unique().notNullable();
-    table.string('name', 100).unique().notNullable();
+    table.string('name', 255).unique().notNullable();
     table.dateTime('created_on').defaultTo(knex.fn.now());
     table.dateTime('modified_on').defaultTo(knex.fn.now());
   });
--- db/seeds/development/06_set.js
+++ db/seeds/development/06_set.js
@@ -3,83 +3,83 @@ exports.seed = function (knex) {
   return knex('set').del()
     .then(() => knex('set').insert([
       {
-        workout_id: 1, weight: 45, number_of_sets: 2, number_of_reps: 5, note: 'Easy warmup',
+        user_id: 1, workout_id: 1, weight: 45, number_of_sets: 2, number_of_reps: 5, note: 'Easy warmup',
       },
       {
-        workout_id: 1, weight: 135, number_of_sets: 1, number_of_reps: 5, note: 'Easy warmup',
+        user_id: 1, workout_id: 1, weight: 135, number_of_sets: 1, number_of_reps: 5, note: 'Easy warmup',
       },
       {
-        workout_id: 1, weight: 145, number_of_sets: 1, number_of_reps: 3, note: 'Easy warmup',
+        user_id: 1, workout_id: 1, weight: 145, number_of_sets: 1, number_of_reps: 3, note: 'Easy warmup',
       },
       {
-        workout_id: 1, weight: 155, number_of_sets: 1, number_of_reps: 2, note: 'Easy warmup',
+        user_id: 1, workout_id: 1, weight: 155, number_of_sets: 1, number_of_reps: 2, note: 'Easy warmup',
       },
       {
-        workout_id: 1, weight: 165, number_of_sets: 3, number_of_reps: 5, note: 'Can go up next week.',
+        user_id: 1, workout_id: 1, weight: 165, number_of_sets: 3, number_of_reps: 5, note: 'Can go up next week.',
       },
       {
-        workout_id: 2, weight: 45, number_of_sets: 2, number_of_reps: 5, note: 'Easy warmup',
+        user_id: 1, workout_id: 2, weight: 45, number_of_sets: 2, number_of_reps: 5, note: 'Easy warmup',
       },
       {
-        workout_id: 2, weight: 135, number_of_sets: 1, number_of_reps: 5, note: 'Easy warmup',
+        user_id: 1, workout_id: 2, weight: 135, number_of_sets: 1, number_of_reps: 5, note: 'Easy warmup',
       },
       {
-        workout_id: 2, weight: 145, number_of_sets: 1, number_of_reps: 3, note: 'Easy warmup',
+        user_id: 1, workout_id: 2, weight: 145, number_of_sets: 1, number_of_reps: 3, note: 'Easy warmup',
       },
       {
-        workout_id: 2, weight: 155, number_of_sets: 1, number_of_reps: 2, note: 'Easy warmup',
+        user_id: 1, workout_id: 2, weight: 155, number_of_sets: 1, number_of_reps: 2, note: 'Easy warmup',
       },
       {
-        workout_id: 2, weight: 165, number_of_sets: 3, number_of_reps: 5, note: 'Shoulder hurt, go down next week.',
+        user_id: 1, workout_id: 2, weight: 165, number_of_sets: 3, number_of_reps: 5, note: 'Shoulder hurt, go down next week.',
       },
       {
-        workout_id: 3, weight: 135, number_of_sets: 1, number_of_reps: 5, note: 'Easy warmup',
+        user_id: 1, workout_id: 3, weight: 135, number_of_sets: 1, number_of_reps: 5, note: 'Easy warmup',
       },
       {
-        workout_id: 3, weight: 145, number_of_sets: 1, number_of_reps: 5, note: 'Easy warmup',
+        user_id: 1, workout_id: 3, weight: 145, number_of_sets: 1, number_of_reps: 5, note: 'Easy warmup',
       },
       {
-        workout_id: 3, weight: 155, number_of_sets: 1, number_of_reps: 3, note: 'Grip was slipping',
+        user_id: 1, workout_id: 3, weight: 155, number_of_sets: 1, number_of_reps: 3, note: 'Grip was slipping',
       },
 
       {
-        workout_id: 4, weight: 45, number_of_sets: 2, number_of_reps: 5, note: 'Easy warmup',
+        user_id: 2, workout_id: 4, weight: 45, number_of_sets: 2, number_of_reps: 5, note: 'Easy warmup',
       },
       {
-        workout_id: 4, weight: 135, number_of_sets: 1, number_of_reps: 5, note: 'Easy warmup',
+        user_id: 2, workout_id: 4, weight: 135, number_of_sets: 1, number_of_reps: 5, note: 'Easy warmup',
       },
       {
-        workout_id: 4, weight: 145, number_of_sets: 1, number_of_reps: 3, note: 'Easy warmup',
+        user_id: 2, workout_id: 4, weight: 145, number_of_sets: 1, number_of_reps: 3, note: 'Easy warmup',
       },
       {
-        workout_id: 4, weight: 155, number_of_sets: 1, number_of_reps: 2, note: 'Easy warmup',
+        user_id: 2, workout_id: 4, weight: 155, number_of_sets: 1, number_of_reps: 2, note: 'Easy warmup',
       },
       {
-        workout_id: 4, weight: 165, number_of_sets: 3, number_of_reps: 5, note: 'Can go up next week.',
+        user_id: 2, workout_id: 4, weight: 165, number_of_sets: 3, number_of_reps: 5, note: 'Can go up next week.',
       },
       {
-        workout_id: 5, weight: 45, number_of_sets: 2, number_of_reps: 5, note: 'Easy warmup',
+        user_id: 2, workout_id: 5, weight: 45, number_of_sets: 2, number_of_reps: 5, note: 'Easy warmup',
       },
       {
-        workout_id: 5, weight: 135, number_of_sets: 1, number_of_reps: 5, note: 'Easy warmup',
+        user_id: 2, workout_id: 5, weight: 135, number_of_sets: 1, number_of_reps: 5, note: 'Easy warmup',
       },
       {
-        workout_id: 5, weight: 145, number_of_sets: 1, number_of_reps: 3, note: 'Easy warmup',
+        user_id: 2, workout_id: 5, weight: 145, number_of_sets: 1, number_of_reps: 3, note: 'Easy warmup',
       },
       {
-        workout_id: 5, weight: 155, number_of_sets: 1, number_of_reps: 2, note: 'Easy warmup',
+        user_id: 2, workout_id: 5, weight: 155, number_of_sets: 1, number_of_reps: 2, note: 'Easy warmup',
       },
       {
-        workout_id: 5, weight: 165, number_of_sets: 3, number_of_reps: 5, note: 'Shoulder hurt, go down next week.',
+        user_id: 2, workout_id: 5, weight: 165, number_of_sets: 3, number_of_reps: 5, note: 'Shoulder hurt, go down next week.',
       },
       {
-        workout_id: 6, weight: 135, number_of_sets: 1, number_of_reps: 5, note: 'Easy warmup',
+        user_id: 2, workout_id: 6, weight: 135, number_of_sets: 1, number_of_reps: 5, note: 'Easy warmup',
       },
       {
-        workout_id: 6, weight: 145, number_of_sets: 1, number_of_reps: 5, note: 'Easy warmup',
+        user_id: 2, workout_id: 6, weight: 145, number_of_sets: 1, number_of_reps: 5, note: 'Easy warmup',
       },
       {
-        workout_id: 6, weight: 155, number_of_sets: 1, number_of_reps: 3, note: 'Grip was slipping',
+        user_id: 2, workout_id: 6, weight: 155, number_of_sets: 1, number_of_reps: 3, note: 'Grip was slipping',
       },
     ]));
 };
