fcec88c667b7f3985a2404636b90b069a66f1591
Alex Puhl <alexpuhldeveloper@gmail.com>
2021-07-25 19:29:50 -0600

    Added a lot more seed data to work with during development

--- db/seeds/development/02_user.js
+++ db/seeds/development/02_user.js
@@ -1,8 +1,10 @@
+/* eslint-disable func-names */
 exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('user').del()
     .then(() => knex('user').insert([
       { role_id: 1, username: 'superUser', password: '$2a$10$K9O.scGTAA4h5Xe23JKBWO1ig492kSvw/w8e6CfKIf6fkdcaXlxqW' },
+      // WE WILL DELETE THESE TWO ROLES AFTER LAUNCH
       { role_id: 1, username: 'alex', password: '$2a$10$qJiaHXU1wInqFFOsOMmB0eAfsZt4XB97VGZapb0OLJqMTX/Nbi7iu' },
       { role_id: 1, username: 'chris', password: '$2a$10$qJiaHXU1wInqFFOsOMmB0eAfsZt4XB97VGZapb0OLJqMTX/Nbi7iu' },
     ]));
