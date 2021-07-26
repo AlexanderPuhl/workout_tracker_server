11ce4e27f98ff94a10db7169e0f1c0ed82f09005
Alex Puhl <alexpuhldeveloper@gmail.com>
2021-07-25 18:30:19 -0600

    Added a super user to the seed data

--- db/seeds/development/02_user.js
+++ db/seeds/development/02_user.js
@@ -2,6 +2,7 @@ exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('user').del()
     .then(() => knex('user').insert([
+      { role_id: 1, username: 'superUser', password: '$2a$10$K9O.scGTAA4h5Xe23JKBWO1ig492kSvw/w8e6CfKIf6fkdcaXlxqW' },
       { role_id: 1, username: 'alex', password: '$2a$10$qJiaHXU1wInqFFOsOMmB0eAfsZt4XB97VGZapb0OLJqMTX/Nbi7iu' },
       { role_id: 1, username: 'chris', password: '$2a$10$qJiaHXU1wInqFFOsOMmB0eAfsZt4XB97VGZapb0OLJqMTX/Nbi7iu' },
     ]));
