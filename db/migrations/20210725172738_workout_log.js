945b9ccd0b9071dcf29b8d92c412598ee1bf3be0
Alex Puhl <alexpuhldeveloper@gmail.com>
2021-08-05 20:31:55 -0600

    Updated the migration files so that certain tables will delete on cascade

--- db/migrations/20210725172738_workout_log.js
+++ db/migrations/20210725172738_workout_log.js
@@ -2,7 +2,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('workout_log', (table) => {
     table.increments('workout_log_id').unique().notNullable();
-    table.integer('user_id').references('user_id').inTable('user');
+    table.integer('user_id').references('user_id').inTable('user').onDelete('CASCADE');
     table.string('note', 255);
     table.dateTime('created_on').defaultTo(knex.fn.now());
     table.dateTime('modified_on').defaultTo(knex.fn.now());
--- db/migrations/20210725174931_set.js
+++ db/migrations/20210725174931_set.js
@@ -3,7 +3,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('set', (table) => {
     table.increments('set_id').unique().notNullable();
     table.integer('user_id').references('user_id').inTable('user');
-    table.integer('workout_id').references('workout_id').inTable('workout');
+    table.integer('workout_id').references('workout_id').inTable('workout').onDelete('CASCADE');
     table.integer('number_of_sets');
     table.integer('number_of_reps');
     table.integer('weight');
