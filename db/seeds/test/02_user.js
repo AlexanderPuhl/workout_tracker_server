2ccd84808ab5f4fda22b622936185830da1d6910
Alexander Puhl <alexpuhldeveloper@gmail.com>
2021-08-26 13:33:25 -0600

    Updated the user seed files to include email/name

--- db/seeds/test/02_user.js
+++ db/seeds/test/02_user.js
@@ -3,9 +3,15 @@ exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('user').del()
     .then(() => knex('user').insert([
-      { role_id: 1, username: 'superUser', password: '$2a$10$qJiaHXU1wInqFFOsOMmB0eAfsZt4XB97VGZapb0OLJqMTX/Nbi7iu' },
+      {
+        role_id: 1, username: 'superUser', email: 'super@test.com', name: 'Super User', password: '$2a$10$K9O.scGTAA4h5Xe23JKBWO1ig492kSvw/w8e6CfKIf6fkdcaXlxqW',
+      },
       // WE WILL DELETE THESE TWO ROLES AFTER LAUNCH
-      { role_id: 1, username: 'alex', password: '$2a$10$qJiaHXU1wInqFFOsOMmB0eAfsZt4XB97VGZapb0OLJqMTX/Nbi7iu' },
-      { role_id: 1, username: 'chris', password: '$2a$10$qJiaHXU1wInqFFOsOMmB0eAfsZt4XB97VGZapb0OLJqMTX/Nbi7iu' },
+      {
+        role_id: 1, username: 'alex', email: 'alex@test.com', name: 'Alex Puhl', password: '$2a$10$qJiaHXU1wInqFFOsOMmB0eAfsZt4XB97VGZapb0OLJqMTX/Nbi7iu',
+      },
+      {
+        role_id: 1, username: 'chris', email: 'chris@test.com', name: 'Chris Gonzalez', password: '$2a$10$qJiaHXU1wInqFFOsOMmB0eAfsZt4XB97VGZapb0OLJqMTX/Nbi7iu',
+      },
     ]));
 };
