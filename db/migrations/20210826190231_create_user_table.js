7a87bc178fb8404f5d71425baa1ac0820197dc5c
Alexander Puhl <alexpuhldeveloper@gmail.com>
2021-08-26 13:12:03 -0600

    Added username/email columns to the user table

--- db/migrations/20210826190231_create_user_table.js
+++ db/migrations/20210826190231_create_user_table.js
@@ -3,7 +3,9 @@ exports.up = function (knex) {
   return knex.schema.createTable('user', (table) => {
     table.increments('user_id').unique().notNullable();
     table.integer('role_id').notNullable().references('role_id').inTable('role');
-    table.string('username', 35).notNullable();
+    table.string('username', 35).unique().notNullable();
+    table.string('email', 70).unique().notNullable();
+    table.string('name', 70).notNullable();
     table.string('password', 255).notNullable();
     table.dateTime('last_login').defaultTo(knex.fn.now());
     table.dateTime('created_on').defaultTo(knex.fn.now());
